6dffcdaf63c33e21e9bec78670ddbb3efb90a9f2
liyong <leonpa1987@gmail.com>
2022-06-13 22:31:14 +0800

    add soft time limit

--- tasks.py
+++ tasks.py
@@ -1,19 +1,24 @@
 # tasks.py
 from celery import Celery
+from celery.exceptions import SoftTimeLimitExceeded
 
-# Celery 第一个参数是当前module的名称，第二个参数是boker的关键字
-# Redis you can use redis://localhost
-#app = Celery('tasks', broker='pyamqp://guest@localhost//')
-# or
 app = Celery('tasks', backend='redis://localhost', broker='redis://localhost//')
 app.conf.broker_url = 'redis://localhost:6379/0'
 
-
-@app.task(time_limit=10)
-def add(x, y):
+@app.task(name='add_with_hard_timeout', time_limit=10)
+def add_with_hard_timeout(x, y):
     import time
     time.sleep(20)
-    print('正常处理')
-
+    print('Normal process')
     return x + y
 
+
+@app.task(name='add_with_soft_timeout', soft_time_limit=10)
+def add_with_soft_timeout(x,y):
+    try:
+        import time
+        time.sleep(20)
+        print('Normal process')
+        return x+y
+    except SoftTimeLimitExceeded:
+        print('Timeout process')
